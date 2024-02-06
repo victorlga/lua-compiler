04766c9a51171744b5edc963fb60c46dba123883
Victor Assis <victorluisgassis@gmail.com>
2024-02-05 21:52:15 -0300

    complete syntactic analysis

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from dataclasses import dataclass
 class Compiler:
 
     def __init__(self, arithmetic_op):
-        self.arithmetic_op = arithmetic_op
-        self.len_arith_op = len(arithmetic_op)
+        self.arithmetic_op = arithmetic_op.strip()
+        self.len_arith_op = len(self.arithmetic_op)
         self.result = None
-        self._lexical_res = None
-        self._syntactic_res = None
-        self._semantic_res = None
+        self._tokens = None
 
     def lexical(self):
         valid_characters = (' ', '+', '-', '1', '2', '3', '4', '5', '6', '7', '8', '9')
@@ -45,12 +43,49 @@ class Compiler:
             prev_char = char
             prev_char_type = define_char_type(prev_char)
 
-        tokens += token
-
-        self._lexical_res = tokens
+        tokens += [token]
+        self.tokens = tokens
 
     def syntactic(self):
-        return
+
+        def is_int(s):
+            try:
+                int(s)  # Try converting the string to an integer
+                return True
+            except ValueError:
+                return False
+
+        def define_token_type(token):
+            if is_int(token):
+                return 'number'
+            if ' ' in token:
+                return 'space'
+            return 'operation'
+
+        prev_token = self.tokens[0]
+        prev_token_type = define_token_type(prev_token)
+        if prev_token_type == 'operation':
+            raise ValueError()
+        num_foll_space = False
+
+        for i in range(1, len(self.tokens)):
+            token = self.tokens[i]
+            token_type = define_token_type(token)
+
+            if token_type == prev_token_type:
+                raise ValueError()
+
+            if num_foll_space:
+                if token_type == 'number':
+                    raise ValueError()
+
+            num_foll_space = prev_token_type == 'number' and token_type == 'space'
+
+            prev_token = token
+            prev_token_type = token_type
+
+        if token_type == 'operation':
+            raise ValueError()
 
     def semantic(self):
         return
@@ -63,5 +98,6 @@ if __name__ == "__main__":
     compiler.lexical()
     compiler.syntactic()
     compiler.semantic()
+    compiler.calculate()
 
-    print(compiler._lexical_res)
+    print(compiler.result)
