96170a4aa34a175510f6da139ce5ce94ae79f1ff
Victor Assis <victor.assis@hey.com>
2024-04-08 18:38:14 -0300

    implement if and while statements

--- syntactical.py
+++ syntactical.py
@@ -43,8 +43,8 @@ class Parser:
             assigment_node.children.append(identifier_node)
 
             self.tokenizer.select_next()
-            expression = self._parse_expression()
-            assigment_node.children.append(expression)
+            bool_expression = self._parse_bool_expression()
+            assigment_node.children.append(bool_expression)
 
             token = self.tokenizer.next
             if token.type != 'NEWLINE':
@@ -61,8 +61,8 @@ class Parser:
                 raise ValueError('Expected OPEN_PAR token type, got: ' + token.type)
 
             self.tokenizer.select_next()
-            expression = self._parse_expression()
-            print_node.children.append(expression)
+            bool_expression = self._parse_bool_expression()
+            print_node.children.append(bool_expression)
 
             token = self.tokenizer.next
             if token.type != 'CLOSE_PAR':
@@ -74,6 +74,76 @@ class Parser:
                 raise ValueError('Expected NEWLINE token type, got: ' + token.type)
 
             return print_node
+
+        elif token.type == 'WHILE':
+            while_node = WhileNode()
+
+            self.tokenizer.select_next()
+            bool_expression = self._parse_bool_expression()
+            while_node.children.append(bool_expression)
+
+            self.tokenizer.select_next()
+            token = self.tokenizer.next
+            if token.type != 'DO':
+                raise ValueError('Expected DO token type, got: ' + token.type)
+            
+            self.tokenizer.select_next()
+            token = self.tokenizer.next
+            if token.type != 'NEWLINE':
+                raise ValueError('Expected NEWLINE token type, got: ' + token.type)
+            
+            block_node = BlockNode()
+            
+            self.tokenizer.select_next()
+            while self.tokenizer.next.type != 'END':
+                statement = self._parse_statement()
+                block_node.children.append(statement)
+                self.tokenizer.select_next()
+
+            while_node.children.append(block_node)
+
+        elif token.type == 'IF':
+            if_node = IfNode()
+
+            self.tokenizer.select_next()
+            bool_expression = self._parse_bool_expression()
+            if_node.children.append(bool_expression)
+
+            self.tokenizer.select_next()
+            token = self.tokenizer.next
+            if token.type != 'THEN':
+                raise ValueError('Expected DO token type, got: ' + token.type)
+            
+            self.tokenizer.select_next()
+            token = self.tokenizer.next
+            if token.type != 'NEWLINE':
+                raise ValueError('Expected NEWLINE token type, got: ' + token.type)
+            
+            block_node = BlockNode()
+            
+            self.tokenizer.select_next()
+            while self.tokenizer.next.type not in ('END', 'ELSE'):
+                statement = self._parse_statement()
+                block_node.children.append(statement)
+                self.tokenizer.select_next()
+
+            if_node.children.append(block_node)
+
+            if (has_else:=self.tokenizer.next.type == 'ELSE'):
+                self.tokenizer.select_next()
+                token = self.tokenizer.next
+                if token.type != 'NEWLINE':
+                    raise ValueError('Expected NEWLINE token type, got: ' + token.type)
+                self.tokenizer.select_next()
+
+            else_block_node = BlockNode()
+
+            while self.tokenizer.next.type != 'END' and has_else:
+                statement = self._parse_statement()
+                else_block_node.children.append(statement)
+                self.tokenizer.select_next()
+
+            if_node.children.append(else_block_node)
 
         token = self.tokenizer.next
         if token.type != 'NEWLINE':
