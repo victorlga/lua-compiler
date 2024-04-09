ebf3d170a62b3128555a93ee62cd8e7b8b1d0d08
Victor Assis <victor.assis@hey.com>
2024-04-09 12:31:53 -0300

    update test.lua

--- test.lua
+++ test.lua
@@ -1,14 +1,37 @@
 
+x_1 = read()
 
+print(x_1)
 
-print(1)
+if (x_1 > 1 and not (x_1 < 1)) or x_1 == 3 then
+	x_1 = 2
+end 
+
 x = 3+6/3   *  2 -+-  +  2*4/2 + 0/1 -((6+ ((4)))/(2)) -- Teste -- Teste 2
 y_1 = 3
-y_1 = y_1 + 2
-
+y_1 = y_1 + x_1
 z__ = x + y_1
 
+if x_1 == 2 then
+	x_1 = 2
+end
+
+if x_1 == 3 then
+	x_1 = 2
+else
+	x_1 = 3
+end
+
+x_1 = 0
+while x_1 < 1 or x == 2 do
+	print(x_1)
+    x_1 = x_1 + 1
+end 
+
+
+
 -- Saida final
+print(x_1)
 print(x)
 print(z__+1)
 
