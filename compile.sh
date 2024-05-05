e1c48523d7908ac42dfa5e20fd19fa5ded2c1938
Victor Assis <victor.assis@hey.com>
2024-05-05 09:59:54 -0300

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,21 @@
-python3 main.py test.lua
-nasm -f elf -o test.o test.asm
-gcc -m32 -no-pie -o test test.o
-./test
+#!/bin/bash
+
+# Extract the filename without the extension
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+# Compile the Lua file
+python3 main.py "$1"
+
+# Assemble with NASM
+nasm -f elf -o "$filename.o" "$filename.asm"
+
+# Link with GCC
+gcc -m32 -no-pie -o "$filename" "$filename.o"
+
+# Run the executable
+./"$filename"
+
+# Remove temporary files
+rm "$filename.o" "$filename" "$filename.asm"
