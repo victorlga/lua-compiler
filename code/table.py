ce8ce91e3c24a1e8a89c6b9b646589e88d82a37c
Victor Assis <victor.assis@hey.com>
2024-05-20 22:25:17 -0300

    move updated symboltable to table.py

--- code/table.py
+++ code/table.py
@@ -2,11 +2,13 @@ class SymbolTable:
 
     def __init__(self):
         self.table = {}
+        self.address = 4
 
     def create(self, key):
         if key in self.table:
             raise RuntimeError(f'Key {key} already created.')
-        self.table[key] = None
+        self.table[key] = (None, None, self.address)
+        self.address += 4
 
     def get(self, key):
         return self.table[key]
